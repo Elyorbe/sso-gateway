2a6308ca252338b26c3b48a5e055bd1a227d51f2
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-10-20 16:00:51 +0900

    Add: Error codes

--- src/main/java/me/elyor/ssogateway/global/error/exception/ErrorCode.java
+++ src/main/java/me/elyor/ssogateway/global/error/exception/ErrorCode.java
@@ -5,8 +5,12 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    ACCESS_DENIED("Access Denied", HttpStatus.FORBIDDEN.value()),
-    UNAUTHORIZED("Unauthorized", HttpStatus.UNAUTHORIZED.value());
+    ACCESS_DENIED("Access denied", HttpStatus.FORBIDDEN.value()),
+    UNAUTHORIZED("Unauthorized", HttpStatus.UNAUTHORIZED.value()),
+    INTERNAL_SERVER_ERROR("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+    BAD_REQUEST("Bad request", HttpStatus.BAD_REQUEST.value()),
+    INVALID_REFRESH_TOKEN("Refresh token is not valid", HttpStatus.BAD_REQUEST.value()),
+    INVALID_JWT("JWT is not valid", HttpStatus.BAD_REQUEST.value());
 
     private final String message;
     private final int status;
--- src/main/java/me/elyor/ssogateway/global/error/exception/GlobalException.java
+++ src/main/java/me/elyor/ssogateway/global/error/exception/GlobalException.java
@@ -3,11 +3,19 @@ package me.elyor.ssogateway.global.error.exception;
 import me.elyor.ssogateway.global.error.ErrorResponse;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class GlobalException extends RuntimeException {
+
     private ErrorResponse errorResponse;
 
     public GlobalException(ErrorCode errorCode) {
         this.errorResponse = ErrorResponse.of(errorCode);
     }
+
+    public GlobalException(ErrorCode errorCode, List<ErrorResponse.FieldError> errors) {
+        this.errorResponse = ErrorResponse.of(errorCode, errors);
+    }
+
 }
